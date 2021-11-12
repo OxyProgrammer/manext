4114da0a0546c63e3dd08ad070751fe837cbd145
Sid <oxyprogrammer@gmail.com>
2021-11-13 00:54:00 +0530

    Checked build, created a more sensible landing page.

--- page-components/Logo.js
+++ page-components/Logo.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-import { makeStyles } from '@mui/styles';
+import Stack from '@mui/material/Stack';
+import { makeStyles, useTheme } from '@mui/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 /*Since I cannot make logos this component is a hack to create a 
   presentable logo for Manext! */
 
@@ -9,33 +11,36 @@ const useStyles = makeStyles((theme) => ({
   brandStyleSolid: {
     fontFamily: 'Raleway',
     color: '#FFFFFF',
+    display: 'inline',
+    fontWeight: 'bold',
   },
   brandStyleTransparent: {
     fontFamily: 'Raleway',
     color: theme.palette.secondary.main,
+    display: 'inline',
+    fontWeight: 'bold',
   },
 }));
 
 const Logo = ({ isSolid }) => {
   const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-    <>
-      <Typography
-        variant='h4'
-        className={
-          classes[`${isSolid ? 'brandStyleSolid' : 'brandStyleTransparent'}`]
-        }
-      >
-        MANEXT
-      </Typography>
+    <Stack direction='row'>
+      {matches ? null : (
+        <Typography
+          variant='h4'
+          className={classes[`${isSolid ? 'brandStyleSolid' : 'brandStyleTransparent'}`]}>
+          MANEXT
+        </Typography>
+      )}
       <Avatar
-        src={`${
-          isSolid
-            ? './partial-logo-solid.svg'
-            : './partial-logo-transparent.svg'
-        }`}
+        className={classes.icon}
+        src={`${isSolid ? './partial-logo-solid.svg' : './partial-logo-transparent.svg'}`}
       />
-    </>
+    </Stack>
   );
 };
 
