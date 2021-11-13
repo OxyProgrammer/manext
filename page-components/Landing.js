d5d5a7f93c3dca871635d145032fdc8b6d57f3c8
Sid <oxyprogrammer@gmail.com>
2021-11-13 19:24:26 +0530

    media query to make landing page look better in landscape mode.

--- page-components/Landing.js
+++ page-components/Landing.js
@@ -2,7 +2,8 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import { makeStyles } from '@mui/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { makeStyles, useTheme } from '@mui/styles';
 import Emoji from 'core-components/Emoji';
 
 const useStyles = makeStyles((theme) => ({
@@ -26,26 +27,29 @@ const useStyles = makeStyles((theme) => ({
 
 const Landing = () => {
   const classes = useStyles();
-
+  const heightMatch = useMediaQuery('(min-height:500px)');
   return (
     <Grid container direction='column'>
       <Grid item>
         <Box className={classes.landingPage}>
-        <Typography variant='h1' className={classes.title}>
-           MANEXT
+          <Typography variant='h1' className={classes.title}>
+            MANEXT
           </Typography>
-          <div sx={{ width: 50, height: 50 }}>
-            <img sx={{ width: '50px' }} src='/question.svg' />
-          </div>
+
+          {heightMatch && (
+            <div sx={{ width: '50px', height: '50px' }}>
+              <img sx={{ width: '50px' }} src='/question.svg' />
+            </div>
+          )}
+
           <Typography variant='h4' className={classes.title}>
             Curious?
           </Typography>
-         
         </Box>
       </Grid>
       <Grid item>
         <Container sx={{ marginTop: '2rem' }}>
-        <Typography variant='h4' className={classes.punchline}>
+          <Typography variant='h4' className={classes.punchline}>
             Manext <Emoji symbol='⏩' />
             is the toolkit to get you started promptly <Emoji symbol='🚀' /> on your real
             work by saving tons of boilerplate. <Emoji symbol='😀' />
