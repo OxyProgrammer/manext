8c441c94cef9c6f4bd3260dba8ca6224472f149c
Sid <oxyprogrammer@gmail.com>
2021-12-04 22:20:33 +0530

    Skeleton code

---
+++ core-components/hooks/useModal.js
@@ -0,0 +1,63 @@
+import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+
+function useModal(open, handleClose, title, content) {
+  const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+      padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+      padding: theme.spacing(1),
+    },
+  }));
+
+  const BootstrapDialogTitle = (props) => {
+    const { children, onClose, ...other } = props;
+
+    return (
+      <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+        {children}
+        {onClose ? (
+          <IconButton
+            aria-label='close'
+            onClick={onClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}>
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </DialogTitle>
+    );
+  };
+
+  return (
+    <BootstrapDialog
+      onClose={handleClose}
+      aria-labelledby='customized-dialog-title'
+      open={open}>
+      <BootstrapDialogTitle id='customized-dialog-title' onClose={handleClose}>
+        {title}
+      </BootstrapDialogTitle>
+      <DialogContent dividers>{content}</DialogContent>
+      <DialogActions>
+        <Button autoFocus onClick={handleClose}>
+          Save changes
+        </Button>
+      </DialogActions>
+    </BootstrapDialog>
+  );
+}
+
+export default useModal;
